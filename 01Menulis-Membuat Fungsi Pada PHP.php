176b3f77bbb05c76fb80ba35690b07fe92146d4b
novlarahmulia <rahmulianovla@gmail.com>
2024-09-17 09:20:37 +0700

    membuat fungsi pada php

---
+++ 01Menulis-Membuat Fungsi Pada PHP.php
@@ -0,0 +1,15 @@
+<?php
+// mmbuat fungsi
+function perkenalan(){
+  echo "Assalamulaikum,<br> ";
+  echo "Perkenalkan, nama saya Novla<br/>";
+  echo "Senang berjumpa dengan anda<br/>";
+}
+// memanggil fungsi yang sudah dibuat
+perkenalan();
+
+echo "<hr>";
+// memanggilnya lagi
+perkenalan();
+?>
+
