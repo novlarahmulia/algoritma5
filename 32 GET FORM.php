13f5fa4f47ac6d31abf288cb74a19fd922304b23
novlarahmulia <rahmulianovla@gmail.com>
2024-10-07 10:23:38 +0700

    GET FROM

--- 32 GET FORM.php
+++ 32 GET FORM.php
@@ -1,7 +1,7 @@
 <form action="proses.php" method="get">
     Nama: <input type="text" name="nama"><br>
     Email: <input type="text" name="email"><br>
-<input type="submit" value="Kirim">
+<input type="submit" value="Kirimm">
 </form>
 
 </body>
