ba60cad4ae0d6dabcbb5263e6557b93b6b72199c
novlarahmulia <rahmulianovla@gmail.com>
2024-10-07 10:12:48 +0700

    menggunakan include dan require di php

--- 17 Menggunakan include dan require di PHP.php
+++ 17 Menggunakan include dan require di PHP.php
@@ -5,7 +5,7 @@ include 'header.php';
 // require
 echo "<hr>";
 require 'config.php';
-// include_once dan require_once
+// include_once dan require_oncee
 echo "<hr>";
 include_once 'library.php';
 require_once 'constants.php';
